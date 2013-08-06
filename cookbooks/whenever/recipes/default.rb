85ab26acf06dfff6291910f6adb3122225f40fd9
ctaymor <ctaymor@gmail.com>
2013-08-06 10:39:52 -0700

    added chef log data to whenever recipe

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -3,16 +3,17 @@ ey_cloud_report "whenever" do
 end
 
 appname="cook-smarts"
-
+Chef::Log.info "Starting whenever recipe"
 if ['solo', 'app-master'].include?(node[:instance_role])
 	local_user=node[:users].first
 	execute "whenever" do
+		Chef::Log.info "Running whenever recipe"
 		cwd "/data/cook_smarts/current"
 		user local_user[:username]
 		command "whenever --update-crontab 'cook_smarts_#{node[:enviroment][:framework_env]}'"
 		action :run
 	end
-
+	Chef::Log.info "Completed whenever recipe"
 	ey_cloud_report "whenever" do
 		message "whenever recipe complete"
 	end
